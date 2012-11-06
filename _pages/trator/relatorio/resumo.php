aefcc5d12c589dbda560a69eb67a9c121d9ebb24
Luis Henrique Soler <blamoo2@hotmail.com>
2012-11-06 16:09:36 -0200

    Adicionados dados do trator no relatório

--- _pages/trator/relatorio/resumo.php
+++ _pages/trator/relatorio/resumo.php
@@ -16,15 +16,9 @@ $PHPWord = new PHPWord();
 // Cria uma nova seção no documento, orientação "Paisagem"
 $section = $PHPWord->createSection(array('orientation'=>'landscape', 'marginLeft'=>300, 'marginRight'=>300, 'marginTop'=>300, 'marginBottom'=>300));
 
+
 // Adiciona um estilo de tabela ao documento
 $PHPWord->addTableStyle('tableStyle', array('borderSize'=>6, 'borderColor'=>'006699', 'cellMargin'=>12));
-
-// Cria uma tabela com o estilo acima e adiciona à seção
-$table = $section->addTable('tableStyle');
-
-// Adiciona nova linha na tabela
-$table->addRow(2400);
-
 
 // Estilo das células do cabeçalho
 $headerCellStyle = new PHPWord_Style_Cell();
@@ -41,30 +35,6 @@ $headerParagraph->setSpaceAfter(12);
 $headerFont = new PHPWord_Style_Font();
 $headerFont->setSize(8);
 
-// Adicionando células do cabeçalho
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Motor (rpm)"),						$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("TDP (rpm)"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Ventilador (rpm)"),					$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 1"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 2"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 1"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 2"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Força (kgf)"),						$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("kW TDP"),								$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("cv TDP"),								$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("kW Motor"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("cv Motor"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("kgf.m TDP"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("N.m TDP"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("kgf.mMotor"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("N.m Motor"),							$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Chv (L.h^-1)"),						$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Chm (kg.h^-1)"),						$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Cs (g.kWh^-1)"),						$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Consumo energético (MJ.h^-1)"),		$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Eficiência (%)"),						$headerFont, $headerParagraph);
-$table->addCell(8000, $headerCellStyle)->addText(utf8_decode("Energia específica (kWh.L^-1)"),		$headerFont, $headerParagraph);
-
 // Estilo das células dos dados
 $dataCellStyle = new PHPWord_Style_Cell();
 $dataCellStyle->setVAlign('center');
@@ -79,32 +49,106 @@ $dataParagraph->setSpaceAfter(60);
 $dataFont = new PHPWord_Style_Font();
 $dataFont->setSize(8);
 
+$tableTrator = $section->addTable('tableStyle');
+
+$tableTrator->addRow();
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Id"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->id), $dataFont, $dataParagraph);
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Nome"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->nome), $dataFont, $dataParagraph);
+
+$tableTrator->addRow();
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Rotação nominal do motor (rpm)"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->rotacao_nominal_motor), $dataFont, $dataParagraph);
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Rotação máxima livre (rpm)"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->rotacao_maxima_livre), $dataFont, $dataParagraph);
+
+$tableTrator->addRow();
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Relação de transmissão Motor/TDP"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->relacao_transmissao_motor), $dataFont, $dataParagraph);
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Relação de transmissão Ventilador/motor"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->relacao_transmissao_ventilador), $dataFont, $dataParagraph);
+
+$tableTrator->addRow();
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Horas do trator"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->horas_trator), $dataFont, $dataParagraph);
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Aspiração"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->aspiracao), $dataFont, $dataParagraph);
+
+$tableTrator->addRow();
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Densidade combustível (kg/L)"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->densidade_combustivel), $dataFont, $dataParagraph);
+
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode("Poder calorífico (MJ/kg)"), $dataFont, $dataParagraph);
+$tableTrator->addCell(8000, $dataCellStyle)->addText(utf8_decode($trator->poder_calorifico), $dataFont, $dataParagraph);
+
+$section->addTextBreak();
+
+// Cria uma tabela com o estilo acima e adiciona à seção
+$tableMedicoes = $section->addTable('tableStyle');
+
+// Adiciona nova linha na tabela
+$tableMedicoes->addRow(2400);
+
+// Adicionando células do cabeçalho
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Motor (rpm)"),						$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("TDP (rpm)"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Ventilador (rpm)"),					$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 1"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 2"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 1"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Medição 2"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Força (kgf)"),						$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("kW TDP"),								$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("cv TDP"),								$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("kW Motor"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("cv Motor"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("kgf.m TDP"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("N.m TDP"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("kgf.mMotor"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("N.m Motor"),							$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Chv (L.h^-1)"),						$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Chm (kg.h^-1)"),						$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Cs (g.kWh^-1)"),						$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Consumo energético (MJ.h^-1)"),		$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Eficiência (%)"),						$headerFont, $headerParagraph);
+$tableMedicoes->addCell(8000, $headerCellStyle)->addText(utf8_decode("Energia específica (kWh.L^-1)"),		$headerFont, $headerParagraph);
+
 // Adicionando células das medições
 foreach($relatorio->dados as $key => $val)
 {
-	$table->addRow();
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->rpm_motor),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->rpm_tdp),					$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->rpm_ventilador),			$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->fm_clp_1),					$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->fm_clp_2),					$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->chv_clp_1),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->chv_clp_2),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->dados_forca),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_kw_tdp),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_cv_tdp),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_kw_motor),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_cv_motor),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_kgfm_tdp),				$dataFont, $dataParagraph);
-    $table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_nm_tdp),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_kgfm_motor),			$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_nm_motor),				$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->cc_chv),					$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->cc_chm),					$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->cc_cs),					$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->consumo_energetico),		$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->eficiencia_termica),		$dataFont, $dataParagraph);
-	$table->addCell(null, $dataCellStyle)->addText(utf8_decode($val->energia_especifica),		$dataFont, $dataParagraph);
+	$tableMedicoes->addRow();
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->rpm_motor),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->rpm_tdp),					$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->rpm_ventilador),			$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->fm_clp_1),					$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->fm_clp_2),					$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->chv_clp_1),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->chv_clp_2),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->dados_forca),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_kw_tdp),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_cv_tdp),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_kw_motor),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->pi_cv_motor),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_kgfm_tdp),				$dataFont, $dataParagraph);
+    $tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_nm_tdp),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_kgfm_motor),			$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->ti_nm_motor),				$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->cc_chv),					$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->cc_chm),					$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->cc_cs),					$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->consumo_energetico),		$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->eficiencia_termica),		$dataFont, $dataParagraph);
+	$tableMedicoes->addCell(null, $dataCellStyle)->addText(utf8_decode($val->energia_especifica),		$dataFont, $dataParagraph);
 }
 // Esses dados provavelmente serão iguais para todas as medições, pegando o valor da última linha somente
 // TODO: passar isso para a tabela de tratores?
